28dc91298d6ac2473a9121ce7d299a2798038e08
elvis <kurtnebiev-elvis4@gmail.com>
2025-04-05 00:45:12 +0200

    feat: Add `bitsandbytes` to dependencies

--- server/task_1.py
+++ server/task_1.py
@@ -1,17 +1,29 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from langdetect import detect
 import torch
 import os
+import gc
 
 UPLOAD_DIR = "uploads"
 
+
+
+gc.collect()
+torch.cuda.empty_cache()
+
 # Загрузка Qwen 14B (Distill)
 model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16
+)
 tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    torch_dtype=torch.float16,
     device_map="auto",
+    quantization_config=bnb_config,
     trust_remote_code=True
 )
 model.eval()
